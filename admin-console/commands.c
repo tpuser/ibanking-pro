6ba3755414d78f1e50c3f5e8438de99356115117
Denis <m-den-i@yandex.by>
2014-11-20 03:45:09 +0300

    Added definitions for new commands

--- admin-console/commands.c
+++ admin-console/commands.c
@@ -1,11 +1,13 @@
 #include "commands.h"
 
-const char *ADMIN_COMMANDS[] = {COMMAND_1};
+const char *ADMIN_COMMANDS[] = {COMMAND_1, COMMAND_TRNSF};
 const char *OPERATOR_COMMANDS[] = {COMMAND_2};
-const char *COMMON_COMMANDS[] = {COMMAND_HELP, COMMAND_EXIT};
-const int ADMIN_COMMANDS_COUNT = 1;
+const char *COMMON_COMMANDS[] = {COMMAND_HELP, COMMAND_EXIT,
+                                 COMMAND_DEB, COMMAND_CRED, COMMAND_CHCK,
+                                 COMMAND_UNDO, COMMAND_COMMIT};
+const int ADMIN_COMMANDS_COUNT = 2;
 const int OPERATOR_COMMANDS_COUNT = 1;
-const int COMMON_COMMANDS_COUNT = 2;
+const int COMMON_COMMANDS_COUNT = 7;
 
 bool commandExists(const char *command)
 {
@@ -20,6 +22,16 @@ bool commandExists(const char *command)
         return true;
     if (!strcmp(command, COMMAND_2))
         return true;
+    if (!strcmp(command, COMMAND_TRNSF))
+        return true;
+    if (!strcmp(command, COMMAND_DEB))
+        return true;
+    if (!strcmp(command, COMMAND_CRED))
+        return true;
+    if (!strcmp(command, COMMAND_CHCK))
+        return true;
+    if (!strcmp(command, COMMAND_UNDO))
+        return true;
 
 
     return false;
@@ -33,6 +45,12 @@ void commandHelp()
     printf("\t%s - command 2 (operator only)\n", COMMAND_2);
     printf("\t%s - display this help\n", COMMAND_HELP);
     printf("\t%s - exit\n", COMMAND_EXIT);
+    printf("\t%s - debit \"id\" \"sum\"\n", COMMAND_DEB);
+    printf("\t%s - credit \"id\" \"sum\"\n", COMMAND_CRED);
+    printf("\t%s - check state \"id\" \n", COMMAND_CHCK);
+    printf("\t%s - transfer \"from_id\" \"to_id\"\ \"sum\"n", COMMAND_TRANSF);
+    printf("\t%s - undo\n", COMMAND_UNDO);
+    printf("\t%s - commit\n", COMMAND_COMMIT);
 }
 
 void command1()
--- admin-console/commands.h
+++ admin-console/commands.h
@@ -14,6 +14,12 @@ static const char COMMAND_1[] = "cmd1";
 static const char COMMAND_2[] = "cmd2";
 static const char COMMAND_HELP[] = "help";
 static const char COMMAND_EXIT[] = "exit";
+static const char COMMAND_DEB[] = "debit";
+static const char COMMAND_CRED[] = "credit";
+static const char COMMAND_CHCK[] = "check";
+static const char COMMAND_TRNSF[] = "transfer";
+static const char COMMAND_UNDO[] = "undo";
+static const char COMMAND_COMMIT[] = "cmit";
 
 static const int ADMIN_GROUP = 0;
 static const int OPERATOR_GROUP = 1;
@@ -25,5 +31,10 @@ bool checkCommandPermission(sqlite3 *db, const char *login, const char *command)
 void commandHelp();
 void command1();
 void command1();
+void debit(int acc_id, double sumtoget);
+void credit(int acc_id, double sumtopush);
+void checkAccount(int acc_id);
+void transfer(int acc_from, int acc_to, double sum);
+void undo();
 
 #endif // COMMANDS_H

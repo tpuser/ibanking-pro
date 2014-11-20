b8d8ce8090ed2a8e38c31ee5aefac914049d9562
Stanislav Belous <programer911@gmail.com>
2014-11-20 14:26:00 +0300

    Commit admin main functions

---
+++ admin-console/admin_command_cust.h
@@ -0,0 +1,8 @@
+#include "sqlite3.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void deleteAccount(sqlite3 *db);
+void createNewCustomer(sqlite3 *db);
+void deleteCustomer(sqlite3 *db);
+void addAccount(sqlite3 *db);

0a7bcca84d84428a5e03fe519bd8c97c3ced0b65
tpuser <pletnevroman95@gmail.com>
2014-12-14 14:26:32 +0300

    added testing

---
+++ admin-console/testing.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sqlite3.h>
+
+#include "bool.h"
+#include "dbinterface.h"
+#include "commands.h"
+#include "admin_command_cust.h"
+
+// for scanf
+#define XSTR(A) STR(A)
+#define STR(A) #A
+
+static const char DEFAULT_DB_FILENAME[] = "../db.sqlite";
+
+void logTestResult(const char *msg, bool result)
+{
+    if (result)
+        printf("test passed: %s\n", msg);
+    else
+        printf("TEST FAILED: %s\n", msg);
+}
+
+int main(int argc, char *argv[])
+{
+    char *dbFilename = NULL;
+    sqlite3 *db;
+    bool status = true;
+
+    // test function commandExists
+    if (!commandExists("help"))
+        status = false;
+    logTestResult("commandExists(\"help\");", status);
+
+    if (commandExists("notexists"))
+        status = false;
+    logTestResult("commandExists(\"notexists\");", status);
+
+    return (!status);
+}
